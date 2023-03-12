9e9daddda695b297a7961d18ce6b5faa383ff7ed
jrfm <moesch.jonas@protonmail.com>
2023-03-12 16:15:58 +0100

    add basic project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles/styles.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="app--container">
+        <h1>Headernpm</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
